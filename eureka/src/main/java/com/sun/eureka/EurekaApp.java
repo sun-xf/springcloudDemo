dd9fed0d2242f39e090b24498ea34b408d14432d
[sunxuefei] <[sunxuefei@sysware.com.cn]>
2019-03-13 10:28:53 +0800

    first commit

---
+++ eureka/src/main/java/com/sun/eureka/EurekaApp.java
@@ -0,0 +1,21 @@
+package com.sun.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *  用来指定该项目为Eureka的服务注册中心
+ * @author Sunxf
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApp 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(EurekaApp.class, args);
+        System.out.println( "---------- EurekaApp start success ! ------------" );
+    }
+}
